10224d1ad15cec3b05c94ac158f02993c6fab546
Abhay Prince <46496449+Abhayprince@users.noreply.github.com>
2024-01-28 22:13:27 +0530

    Set up tabs, navbar style and routing

--- IcecreamMAUI/AppShell.xaml.cs
+++ IcecreamMAUI/AppShell.xaml.cs
@@ -1,9 +1,33 @@
-﻿namespace IcecreamMAUI;
+﻿using IcecreamMAUI.Pages;
+
+namespace IcecreamMAUI;
 
 public partial class AppShell : Shell
 {
     public AppShell()
     {
         InitializeComponent();
+
+        //Routing.RegisterRoute(nameof(SigninPage), typeof(SigninPage));
+        //Routing.RegisterRoute(nameof(SignupPage), typeof(SignupPage));
+
+        RegisterRoutes();        
+    }
+
+    private readonly static Type[] _routablePageTypes = 
+        [
+            typeof(SigninPage),
+            typeof(SignupPage),
+            typeof(MyOrdersPage),
+            typeof(OrderDetailsPage),
+            typeof(DetailsPage),
+        ];
+
+    private static void RegisterRoutes()
+    {
+        foreach (var pageType in _routablePageTypes)
+        {
+            Routing.RegisterRoute(pageType.Name, pageType);
+        }       
     }
 }
--- IcecreamMAUI/Pages/OnboardingPage.xaml.cs
+++ IcecreamMAUI/Pages/OnboardingPage.xaml.cs
@@ -6,4 +6,19 @@ public partial class OnboardingPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+		await Shell.Current.GoToAsync($"//{nameof(HomePage)}");
+    }
+
+    private async void Signin_Clicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync(nameof(SigninPage));
+    }
+
+    private async void Signup_Clicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync(nameof(SignupPage));
+    }
 }
